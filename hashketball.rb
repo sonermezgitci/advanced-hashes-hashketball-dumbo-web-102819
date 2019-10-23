33558afebe4a2be63b81923c65b66976e32a6226
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-23 15:07:21 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,10 @@
+require 'pry'
+
 def game_hash
   {
  :home =>{
-  :team_name =>"Brooklyn Nets ",
-   :colors => ["Black,White"],
+  :team_name =>"Brooklyn Nets",
+   :colors => ["Black", "White"],
    :players =>{
      "Alan Anderson" =>{
    number:0,
@@ -57,8 +59,8 @@ def game_hash
       }
      },
        :away =>{
-         :team_name =>"Charlote Hornets",
-         :colors =>["Turquoise, Purple"],
+         :team_name =>"Charlotte Hornets",
+         :colors =>["Turquoise", "Purple"],
          :players =>{
            "Jeff Adrien" =>{
            number:4,
@@ -67,17 +69,17 @@ def game_hash
            rebounds:1,
            assist:1,
            steals:2,
-           blokcs:7,
+           blocks:7,
            slam_dunks:2
          },
-          "Bismiac Biyombo" =>{
+          "Bismack Biyombo" =>{
             number:0,
             shoe:16,
             points:12,
             rebounds:4,
             assist:7,
             steals:22,
-            blokcs:15,
+            blocks:15,
             slam_dunks:10
             },
            "DeSagna Diop" =>{
@@ -87,7 +89,7 @@ def game_hash
             rebounds:12,
             assist:12,
             steals:4,
-            blokcs:5,
+            blocks:5,
             slam_dunks:5
             },
          "Ben Gordon" =>{
@@ -97,7 +99,7 @@ def game_hash
             rebounds:3,
             assist:2,
             steals:1,
-            blokcs:1,
+            blocks:1,
             slam_dunks:0
             },
           "Kemba Walker" =>{
@@ -107,23 +109,93 @@ def game_hash
             rebounds:12,
             assist:12,
             steals:7,
-            blokcs:5,
+            blocks:5,
             slam_dunks:12
          }
        }
      }
    } 
 end
-       def num_of_points_scored(name)
-         game_hash.each do |location,team_data|
-           team_data[:players].each do |player_name,data|
-             if player_name == name #check to name to match 
-             return_data[:points]
+       def num_points_scored(name)
+         game_hash.each do |team_location,team_hash|
+          
+      team_hash[:players].each do |player_name,player_data|
+        if name == player_name
+          return player_data[:points] 
+      end
+     end
+    end
+   end
+           
+       def shoe_size (name)
+          game_hash.each do |team_location,team_hash|
+            #binding.pry
+        team_hash[:players].each do |player_name,player_data|
+          if name == player_name
+          return player_data[:shoe]
+         end
+       end
+     end
+   end
+        
+def team_colors(team)
+  game_hash.each do |team_position,team_data|
+    #binding.pry
+    if team == team_data[:team_name]
+    return team_data[:colors]
+    #binding.pry
+    end
+  end
+ end
+ 
+ def team_names
+ game_hash.map do |team_position,team_data|
+ #binding.pry
+ team_data[:team_name]
+   end
+  end
+
+def player_numbers(team_name)
+  nums = []
+  game_hash.each do |team_position,team_data|
+    if team[:team_name] == team_name
+      team.each do |attributes,data|
+        if attributes == :players
+          binding.pry
+          data.each do |player|
+            nums << player[:number]
+          end
+        end
       end
     end
   end
+  nums
 end
+  
+  #def player_stats(players_name)    
+    #new_hash -{}
+    #game_hash.each do |team_position,team_data|
+      #team.each do |attributes,data|
+        #if attributes == :players
+          #data.each do |player|
+            #if player[:player_name] == players_name
+              new_hash =player
+        #end 
+      #end 
+     # end
+    #end
+    #end
+  #new_hash
+#end 
+    
 
+ 
+
+ 
+
+    
+       
+    
              
            
 
